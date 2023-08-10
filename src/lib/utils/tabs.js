eb438fe038de44de472fdf2d1a4027cf13b63c36
yamatha <141965614+yamatha@users.noreply.github.com>
2023-08-10 15:39:27 -0700

    Update tabs.js

--- src/lib/utils/tabs.js
+++ src/lib/utils/tabs.js
@@ -5,73 +5,34 @@ export const tabs = [
         label: 'Home',
         dest: '/',
     },
-    {
-        icon: 'sports',
-        label: 'Matchups',
-        dest: '/matchups',
-    },
-    {
-        icon: 'swap_horiz',
-        label: 'Trades & Waivers',
-        dest: '/transactions',
-    },
-    {
-        icon: 'article',
-        label: 'Blog',
-        dest: '/blog',
-    },
-    {
-        icon: 'view_comfy',
-        label: 'League Info',
-        nest: true,
-        children: [
             {
-                icon: 'storage',
                 label: 'Rosters',
                 dest: '/rosters',
             },
             {
-                icon: 'groups',
                 label: 'Managers',
                 dest: '/managers',
             },
             {
-                icon: 'local_fire_department',
-                label: 'Rivalry',
-                dest: '/rivalry',
-            },
-            {
-                icon: 'leaderboard',
                 label: 'Standings',
                 dest: '/standings',
             },
             {
-                icon: 'view_comfy',
                 label: 'Drafts',
                 dest: '/drafts',
             },
             {
-                icon: 'emoji_events',
                 label: 'Trophy Room',
                 dest: '/awards',
             },
             {
-                icon: 'military_tech',
                 label: 'Records',
                 dest: '/records',
             },
             {
-                icon: 'history_edu',
                 label: 'Constitution',
                 dest: '/constitution',
             },
-            {
-                icon: 'sports_football',
-                label: 'Go to Sleeper',
-                dest: `https://sleeper.app/leagues/${leagueID}`,
-            },
-        ]
-    },
     {
         icon: 'lightbulb',
         label: 'Resources',
